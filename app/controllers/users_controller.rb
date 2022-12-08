6c1d3869347ee72940836437a57e1be40aeae53b
Denis4pf <denis.onwonga@student.moringaschool.com>
2022-12-09 02:25:34 +0300

    completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
+
     def show
-        user = User.find_by(id: session[:user_id])
-        if user
-        render json: user, status: :ok
-        else
-            render json: { error: "Not authorized" }, status: :unauthorized
-        end
+     user = User.find_by(id: session[:user_id])
+     render json: user
     end
-end
+    
+   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-  post '/login', to: "sessions#create"
-  delete '/logout', to: "sessions#destroy"
-  get '/me', to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/me", to: "users#show"
 end
